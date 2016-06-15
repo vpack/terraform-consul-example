957b5a0d277e12b8d24507fce5088661753183e9
Venkatesan Packiriswamy <Venkatesan.Packiriswamy@clearme.com>
2016-06-15 11:25:35 -0400

    updated state.tf

--- state.tf
+++ state.tf
@@ -1,14 +1,10 @@
 # Infrastructure State
 
-# Notes:
-#  Always use us-east-1 region for State.
-
 resource "terraform_remote_state" "vpc" {
     backend = "s3"
     config {
-#        bucket = "${format("terraform-tf-states-%x-%x-%x", "${var.environment}" , "${var.project}", "${var.region}")}"
-        bucket = "terraform-tf-states-dev-infra-us-east-1"
-        key = "infra_dev"
-        region = "us-east-1"
+        bucket = "terraform-tf-states-${var.environment}-infra-${var.region}"
+        key = "infra_${var.environment}" 
+        region = "us-east-1"                    #  Always save State file to us-east-1 region.
     }
 }
